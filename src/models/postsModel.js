652cfc8dd9a94a9bb86fa6a08dcb06008c800a93
armanoalves <armanobarrosalvesh@gmail.com>
2024-11-18 14:22:47 -0300

    Aula 4 - conexao com mongodb e separacao de código com o modelo MVC

---
+++ src/models/postsModel.js
@@ -0,0 +1,11 @@
+import conectarAoBanco from "../config/dbConfig.js";
+
+
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
+
+export async function getTodosPosts() {
+    const db = conexao.db("imersão-instabytes");
+    const colecao = db.collection("posts");
+    
+    return colecao.find().toArray();
+}
