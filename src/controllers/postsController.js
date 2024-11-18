daf07717da3c3ebfe9189481e4899e9a9ea8acad
armanoalves <armanobarrosalvesh@gmail.com>
2024-11-18 15:24:19 -0300

    Aula 3 - rotas de criar um novo post e upload de imagem

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,9 +1,35 @@
-import { getTodosPosts } from "../models/postsModel";
-
+import { getTodosPosts, criarPost } from "../models/postsModel";
+import fs from 'fs';
 
 export async function listarPosts(req, res) {
     // Chama a funcao para buscar os posts
     const posts = await getTodosPosts();
     // Envia uma resposta HTTP com status 200 (OK) e os posts
     res.status(200).json(posts);
+}
+
+export async function postarNovoPost(req, res) {
+    const novoPost = req.body;
+
+    try {
+        const postCriado = await criarPost(novoPost);
+        res.status(201).json(postCriado);
+    } catch (erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro":"Falha na requisicao"})
+    }  
+}
+
+export async function postarNovoPost(req, res) {
+    const novoPost = req.body;
+
+    try {
+        const postCriado = await criarPost(novoPost);
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
+        fs.renameSync(req.file.path, imagemAtualizada);
+        res.status(201).json(postCriado);
+    } catch (erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro":"Falha na requisicao"})
+    }  
 }
--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -6,6 +6,13 @@ const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 export async function getTodosPosts() {
     const db = conexao.db("imersão-instabytes");
     const colecao = db.collection("posts");
-    
+
     return colecao.find().toArray();
+}
+
+export async function criarPost(novoPost) {
+    const db = conexao.db("imersão-instabytes");
+    const colecao = db.collection("posts");
+
+    return colecao.insertOne(novoPost);
 }
